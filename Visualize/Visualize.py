5619fd48dab734fa7f796fb485463ce55c071a99
Ali <zamaniali1995@gmail.com>
2018-12-14 15:00:25 +0330

    Complete Visualization and report of it

--- Visualize/Visualize.py
+++ Visualize/Visualize.py
@@ -19,14 +19,14 @@ print('Loading the dataset.....')
 credit_card = pd.read_csv('../dataSet/creditcard.csv')
 print('Dataset shape: ',credit_card.shape)
 print('Dataset was loaded!!!')
-#%%Plot fraud vs nonfraud and heatmap
+#%%Plot fraud vs nonfraud
 f, ax = plt.subplots(figsize=(7, 5))
 sns.countplot(x='Class', data=credit_card)
 _ = plt.title('# Fraud vs NonFraud')
 _ = plt.xlabel('Class (1==Fraud)')   
 plt.savefig("fraudvsnonfraud"+".pdf")
 plt.show()
-#%%
+#%%Heatmap
 corr=credit_card.corr()
 mask = np.zeros_like(corr, dtype=np.bool)
 mask[np.triu_indices_from(mask)] = True
@@ -37,11 +37,10 @@ sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
             square=True, linewidths=.5, cbar_kws={"shrink": .5})
 plt.savefig("heatmap"+".pdf")
 plt.show()
-#%%Non fraud data describe
+#%%Fraud and non-fraud data describe
 non_fraud = credit_card[credit_card.Class == 0]
-non_fraud.Amount.describe()
-#%%fraud data describe
 fraud = credit_card[credit_card.Class == 1]
+non_fraud.Amount.describe()
 fraud.Amount.describe()
 #%%plot of high value transactions
 bins = np.linspace(200, 2500, 100)
@@ -51,7 +50,7 @@ plt.legend(loc='upper right')
 plt.title("Amount by percentage of transactions (transactions \$200+)")
 plt.xlabel("Transaction amount (USD)")
 plt.ylabel("Percentage of transactions (%)");
-plt.savefig("Amount by percentage of transactions"+".pdf")
+plt.savefig("Amountbypercentageoftransactions"+".pdf")
 plt.show()
 #%%Visual Exploration of the Data by Hour
 bins = np.linspace(0, 48, 48) #48 hours
@@ -61,7 +60,7 @@ plt.legend(loc='upper right')
 plt.title("Percentage of transactions by hour")
 plt.xlabel("Transaction time as measured from first transaction in the dataset (hours)")
 plt.ylabel("Percentage of transactions (%)");
-plt.savefig("Visual Exploration of the Data by Hour"+".pdf")
+plt.savefig("VisualExplorationoftheDatabyHour"+".pdf")
 plt.show()
 #%%Visual Exploration of Transaction Amount vs. Hour
 plt.scatter((non_fraud.Time/(60*60)), non_fraud.Amount, alpha=0.6, label='non-fraud')
@@ -70,5 +69,5 @@ plt.title("Amount of transaction by hour")
 plt.xlabel("Transaction time as measured from first transaction in the dataset (hours)")
 plt.ylabel('Amount (USD)')
 plt.legend(loc='upper right')
-plt.savefig("Visual Exploration of Transaction Amount vs. Hour"+".pdf")
+plt.savefig("VisualExplorationofTransactionAmountvsHour"+".pdf")
 plt.show()
